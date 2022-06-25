54eb066473dada9cc8b8059173bd36351ba6a200
Alena <kudryashova_a_n@example.com>
2022-06-25 15:51:56 +0300

    DopDz_Zadacha2

---
+++ DopDz_Zadacha2/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается*/
+
+while(true){
+    Console.Write("Введите exit для завершения.");
+    string exit = Console.ReadLine();
+    if (exit.Equals("exit")){
+        return;
+    }
+}
