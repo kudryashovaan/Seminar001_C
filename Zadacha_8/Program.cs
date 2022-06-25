7e8e1a3130cd937788e5bf6661e775bdc42c005f
Alena <kudryashova_a_n@example.com>
2022-06-25 13:20:18 +0300

    zadacha 8

---
+++ Zadacha_8/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int even = 1;
+while(even<=N){
+    if (even % 2 == 0){
+        Console.Write(even + " ,");
+    }
+    even++;
+}
