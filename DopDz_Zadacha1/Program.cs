d72ae7cfa629d6d3119af8306a2893b308bad5bd
Alena <kudryashova_a_n@example.com>
2022-06-25 15:39:06 +0300

    DopDz_Zadacha1

---
+++ DopDz_Zadacha1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*В переменной string есть секретное сообщение, во второй есть пароль. 
+Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
+
+string message = "Секретное сообщение";
+string password = "1234";
+
+Console.Write("Введите пароль, у вас 3 попытки: ");
+
+string FirstTry = Console.ReadLine();
+
+if (FirstTry.Equals(password)){
+    Console.WriteLine(message);
+    return;
+}
+else {Console.WriteLine("Введите пароль еще раз. осталос 2 попытки!");}
+
+string SecondTry = Console.ReadLine();
+
+if (SecondTry.Equals(password)){
+    Console.WriteLine(message);
+    return;
+}
+else {Console.WriteLine("Введите пароль еще раз. осталос 1 попытка!");}
+
+string ThirdTry = Console.ReadLine();
+
+if (ThirdTry.Equals(password)){
+    Console.WriteLine(message);
+    return;
+}
+else {
+    Console.WriteLine("Попытки ввести пароль закончились!");
+}
