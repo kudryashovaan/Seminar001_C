2101c36894e1e1293868a60fdd97fbbb88be355e
Alena <kudryashova_a_n@example.com>
2022-06-26 18:01:35 +0300

    Zadacha_10

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number<100 || number>999){
+    Console.Write("Введите трехзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+int third = number/10;
+int second = third%10;
+Console.WriteLine(second);
+
