cfe1412a7f28799d95f794ea1690d218bd6a1b0c
Alena <kudryashova_a_n@example.com>
2022-07-02 12:44:48 +0300

    21

--- Zadacha_21/Program.cs
+++ Zadacha_21/Program.cs
@@ -2,7 +2,10 @@
 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
-int InputInt(string output) {     Console.Write(output);     return Convert.ToInt32(Console.ReadLine()); }
+int InputInt(string output) {     
+    Console.Write(output);     
+    return Convert.ToInt32(Console.ReadLine()); 
+}
 
 int ax1 = InputInt("Введите координаты первой точки ax1: ");
 int ay1 = InputInt("Введите координаты первой точки ay1: ");
