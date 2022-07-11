3d8d5f2553698f904c39f5c6f6d6e71a5e0e17d0
Alena <kudryashova_a_n@example.com>
2022-07-11 22:24:33 +0300

    43

--- Zadacha_43/Program.cs
+++ Zadacha_43/Program.cs
@@ -12,5 +12,7 @@ int k1 = InputInt("Введите k1: ");
 int b2 = InputInt("Введите b2: ");
 int k2 = InputInt("Введите k2: ");
 
-Console.WriteLine();
+double x = (double)(b2-b1)/(k1-k2);
+double y = (double)(k1*x+b1);
+Console.WriteLine("Точка пересечения двух прямых: " + x + "; " + y);
 
