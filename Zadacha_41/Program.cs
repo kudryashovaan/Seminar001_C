f89538bfe6bda46cae76f0d9b590893d47285b98
Alena <kudryashova_a_n@example.com>
2022-07-12 22:57:34 +0300

    41

--- Zadacha_41/Program.cs
+++ Zadacha_41/Program.cs
@@ -1,24 +1,20 @@
 ﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3*/
-
 Console.WriteLine("Введите числа через запятую: ");
 string str = Console.ReadLine();
-string strings = str.Replace(",", "");
-char[] ch = new char[strings.Length];
-ch = strings.ToCharArray();
-int[] number = new int [ch.Length];
 
-for (int i=0; i<ch.Length; i++){
-    number[i] = Convert.ToInt32(new string(ch[i], 1));
-    Console.Write(number[i] + " ");
-}
 Console.WriteLine();
 int count = 0;
-for (int i=0; i<number.Length; i++){
-   if (number[i]>0){
-       count++;
-   }
+string x = "-";
+for (int i = 0; i < str.Length; i++){   
+    if ((i+2)<str.Length && str[i].Equals(x.ToString()))
+        i=i+2;
+    if (char.IsDigit(str[i]))    
+    {       
+        if (str[i] > '0') 
+        count++;
+    }
 }
 Console.WriteLine("Чисел больше 0: " + count);
 
