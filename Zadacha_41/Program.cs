cd5ba5b21209cd2ac70bca718c06eb162eef936d
Alena <kudryashova_a_n@example.com>
2022-07-13 21:43:24 +0300

    41

--- Zadacha_41/Program.cs
+++ Zadacha_41/Program.cs
@@ -3,17 +3,21 @@
 1, -7, 567, 89, 223-> 3*/
 Console.WriteLine("Введите числа через запятую: ");
 string str = Console.ReadLine();
-
 Console.WriteLine();
 int count = 0;
-string x = "-";
-for (int i = 0; i < str.Length; i++){   
-    if ((i+2)<str.Length && str[i].Equals(x.ToString()))
-        i=i+2;
-    if (str[i] > '0' && i + 1 < str.Length && char.IsDigit(str[i + 1]))
-        //xz
-    if (char.IsDigit(str[i]))    
-    {       
+
+for (int i=0; i < str.Length; i++){   
+    if ((i+1)<str.Length && char.IsDigit(str[i]) && str[i] == '-'){
+        continue;
+    }
+    while ((i+1) < str.Length && char.IsDigit(str[i+1])) {
+        i++;
+        if (!char.IsDigit(str[i+1]) && i+1 < str.Length){
+        count++;
+        continue;
+        }
+    } 
+    if (char.IsDigit(str[i])){       
         if (str[i] > '0') 
         count++;
     }
