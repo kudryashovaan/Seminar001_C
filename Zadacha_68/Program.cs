cc1edd6f2b1931c5f8e35bff27467915e5d25daa
Alena <kudryashova_a_n@example.com>
2022-07-21 19:58:10 +0300

    68

--- Zadacha_68/Program.cs
+++ Zadacha_68/Program.cs
@@ -3,17 +3,17 @@
 Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 29*/
 
-int AkkermanFun(int n, int m){
-    if (n == 0){ 
-        return m + 1;
+int AkkermanFun(int m, int n){
+    if (m == 0){ 
+        return n + 1;
     }
-    if (n != 0 && m == 0){ 
-        return AkkermanFun(n - 1, 1);
+    if (m != 0 && n == 0){ 
+        return AkkermanFun(m - 1, 1);
     }
-    if (n > 0 && m > 0){ 
-        return AkkermanFun(n - 1, AkkermanFun(n, m - 1));
+    if (m > 0 && n > 0){ 
+        return AkkermanFun(m - 1, AkkermanFun(m, n - 1));
     }
-    return AkkermanFun(n,m);
+    return AkkermanFun(m,n);
 }
     
 Console.WriteLine(AkkermanFun(2, 3));
